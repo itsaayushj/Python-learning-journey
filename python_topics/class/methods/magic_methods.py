b414f0a7697b9bdc0e1662e36d0da93e244655d4
Aayush Jha <a2jha1909@gmail.com>
2026-04-05 20:41:05 +0530

    feat : add __repr__ and fix multiple typos

--- python_topics/class/methods/magic_methods.py
+++ python_topics/class/methods/magic_methods.py
@@ -9,7 +9,7 @@ class Book :
         self.num_pages = num_pages
 
     def __str__(self) : 
-        return f"{self.book} by {self.author}"
+        return f"{self.title} by {self.author}"
     
     def __eq__(self , other) : 
         return self.title == other.title and self.author == other.author
@@ -27,13 +27,14 @@ class Book :
         if key == "Title" : 
             return self.title
         elif key == "Author" : 
-            return self.authot 
+            return self.author 
         elif key == "num_pages" : 
             return self.num_pages
         else :
             return f"{key} Not found"
 
-
+    def __repr__(self) : 
+        return f"'{self.title}' by {self.author} of {self.num_pages} pages"
 
 book1 = Book("The Alcemist" , "Paulo Coelho" , 100)
 book2 = Book("Paradise Lost" , "John Milton" , 300)
@@ -44,3 +45,7 @@ print(book1 > book2) #testing __gt__
 print(book1 < book2) #testing __lt__
 print("Paulo" in book1 ) # testing __contains__
 print(book1['num_pages']) # testing _getitem__
+# book1  This will return __repr__ only in REPL python or jupyter notebook and not in normal .py file 
+# book2
+print(repr(book1))
+print(book1) # this will print str and if there is no str then repr
