e5f4a48c7429f64f5f6e2ebd5d669ede1aae544a
Aayush Jha <a2jha1909@gmail.com>
2026-04-10 20:52:12 +0530

    chore : fixed some old revision codes

--- python_topics/revisions/basic revisions/dictionary.py
+++ python_topics/revisions/basic revisions/dictionary.py
@@ -1,4 +1,6 @@
 #dictionary 
-formulas = {"square" : number * number }
-float(input("Enter the number"))
+number = float(input("Enter the number : "))
+square = ( number * number)
 print(f"Here is your square {square}")
+
+# this code is incomplete !
