c15e00aceee376846925ab53388607f74617b00c
Aayush Jha <a2jha1909@gmail.com>
2026-04-12 19:58:34 +0530

    feat: add alarm clock exercise

--- python_topics/dates_and_times/alarm_clock/alarm_clock.py
+++ python_topics/dates_and_times/alarm_clock/alarm_clock.py
@@ -2,7 +2,29 @@
 import time 
 import datetime
 import pygame
-import os
 source_sound = "dates_and_times\\alarm_clock\\alarm_tune.mp3"
+def set_alarm(user_input) : 
+    counting_down = True
+    while counting_down :
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        print(current_time)
+        if current_time > user_input : 
+            print("Invalid time!")
+            counting_down = False
+        if current_time == user_input : 
+            print("Time's up!")
+            pygame.mixer.init()
+            pygame.mixer.music.load(source_sound)
+            pygame.mixer.music.play()
+            while pygame.mixer.music.get_busy() : 
+                time.sleep(1)
+            counting_down = False
+        else : 
+            time.sleep(1)
 
-#incomplete - will be done later
+def main() : 
+    user_input = input("Enter alarm time in(HH:MM:SS)")
+    set_alarm(user_input)    
+
+if __name__ == "__main__" : 
+    main()
