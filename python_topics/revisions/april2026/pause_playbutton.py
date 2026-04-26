e106d776ce5e18bae8836ba92a37e08ebfe4b238
Aayush Jha <a2jha1909@gmail.com>
2026-04-26 22:38:05 +0530

    chore: add play pause button design

--- python_topics/revisions/april2026/pause_playbutton.py
+++ python_topics/revisions/april2026/pause_playbutton.py
@@ -1,38 +1,62 @@
-import sys
-from PyQt5.QtWidgets import QApplication , QMainWindow , QLabel , QHBoxLayout , QWidget , QPushButton
+import sys 
+from PyQt5.QtWidgets import QApplication  , QMainWindow , QLabel , QWidget , QHBoxLayout , QPushButton
 from PyQt5.QtCore import Qt
-
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.resize(600 , 300)
+        self.resize(800 , 300)
         self.initui()
+        self.setWindowTitle("Play , Pause , Stop buttons in Pyqt5")
     def initui(self):
-            self.endbutton = QPushButton("■")
-            self.pausebutton = QPushButton("▶")
-            central_widget = QWidget()
-            self.setCentralWidget(central_widget)
-            layout = QHBoxLayout()
-            central_widget.setLayout(layout)
-            layout.addWidget(self.endbutton)
-            layout.addWidget(self.pausebutton)
-            self.endbutton.setFixedSize(250 , 250) # to make it circle it needs to be fixed height and width and with resize it can change so we used fixed size
-            self.pausebutton.setFixedSize(250 , 250)
-            self.endbutton.setEnabled(False)
-            self.setStyleSheet("""
-                            QPushButton{
-                               font-size: 80px;
-                               font-family: Arial;
-                                padding: 90px;
-                               margin: 20px;
-                               border: 3px solid;
-                               border-radius: 100px;
-                               }
-                                """)
-
-
+        self.stop_button = QPushButton("⯀")
+        self.play_pause = QPushButton("▶")
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+        layout = QHBoxLayout()
+        central_widget.setLayout(layout)
+        layout.addWidget(self.stop_button  , alignment=(Qt.AlignCenter))
+        layout.addWidget(self.play_pause  , alignment=(Qt.AlignCenter))
+        self.play_pause.setFixedSize(250 , 250)
+        self.stop_button.setFixedSize(250 , 250)
+        self.stop_button.setDisabled(True)
         
-def main():
+        
+        self.setStyleSheet(""" QPushButton{
+                           font-family: Arial;
+                           font-size: 80px;
+                           border: 3px solid;
+                           border-radius: 121px;
+                           }
+                           QPushButton:enabled {
+                           color: black;
+                           background-color: hsl(141, 73%, 55%);
+                           }
+                           QPushButton:disabled {
+                           color: white;
+                           background-color: grey;
+                           }
+                           QPushButton:hover {
+                           font-size: 145px;
+                           background-color: hsl(141, 73%, 65%)
+                           }
+                           QPushButton:pressed {
+                           background-color: hsl(141 , 73% , 40%);
+                           font-size: 110px;
+                           }
+                           """)
+        self.play_pause.clicked.connect(self.play_pause_slot)
+        self.stop_button.clicked.connect(self.stop_slot)
+    def play_pause_slot(self):
+        self.stop_button.setEnabled(True) # so it can be stopped once timer started
+        if self.play_pause.text() == "▶" :
+            self.play_pause.setText("ıı")
+        elif self.play_pause.text() == "ıı" : 
+            self.play_pause.setText("▶")
+    def stop_slot(self):
+        self.stop_button.setDisabled(True) # so it can be stopped once only
+        self.play_pause.setText("▶") # to return it to play if its stopped in resume
+        print("stopped") # for assurance lmao
+def main():             
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
