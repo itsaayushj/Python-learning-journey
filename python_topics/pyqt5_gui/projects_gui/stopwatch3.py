64a76f81843e2a631f24f87c7255e1cf63a2374e
Aayush Jha <a2jha1909@gmail.com>
2026-05-02 20:23:00 +0530

    fix: improved stopwatch with toggle buttons

--- python_topics/pyqt5_gui/projects_gui/stopwatch3.py
+++ python_topics/pyqt5_gui/projects_gui/stopwatch3.py
@@ -10,10 +10,15 @@ class MainWidget(QWidget):
         super().__init__()
         self.resize(700 , 300)
         self.initui()
+        self.button_setter()
+        self.time = QTime(0 , 0 , 0 ,0) # hour , minute , second , ms 
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_time)
     def initui(self):
-        self.time_display = QLabel("00:00:00")
+        self.time_display = QLabel("00:00:00.00")
         self.play_pause_button = QPushButton("▶")
         self.reset_button = QPushButton("⯀")
+        self.reset_button.setDisabled(True)
         vbox = QVBoxLayout()
         hbox = QHBoxLayout()
         vbox.addWidget(self.time_display , alignment=(Qt.AlignCenter))
@@ -39,12 +44,42 @@ class MainWidget(QWidget):
                            }
                            QLabel{
                             font-family: Courier New;
-                            font-size: 70px
-                            font-color: #cbcbcb
+                            font-size: 90px;
+                            color: #cbcbcb;
+                           font-weight: bold;
                            }
                            
                            """)
-        
+    def button_setter(self):
+        self.play_pause_button.clicked.connect(self.play_pause_slot)
+        self.reset_button.clicked.connect(self.reset_button_slot)
+    def play_pause_slot(self):
+        self.reset_button.setEnabled(True)
+        if self.play_pause_button.text() == "▶":
+            self.timer.start(10)
+            self.play_pause_button.setText("ıı")
+        elif self.play_pause_button.text() == "ıı":
+            self.play_pause_button.setText("▶")
+            self.timer.stop()
+            
+    def reset_button_slot(self):
+        self.reset_button.setDisabled(True)
+        self.play_pause_button.setText("▶")
+        self.time = QTime(0 , 0, 0, 0)
+        self.timer.stop()
+        self.time_display.setText(self.time_format(self.time))
+    
+    def time_format(self , time):
+        hours = time.hour()
+        minutes = time.minute()
+        seconds = time.second()
+        milliseconds = time.msec() // 10
+
+        return f"{hours:02}:{minutes:02}:{seconds:02}.{milliseconds:02}"
+    
+    def update_time(self):
+        self.time = self.time.addMSecs(10)
+        self.time_display.setText(self.time_format(self.time))
 def main():
     app = QApplication(sys.argv)
     widget = MainWidget()
